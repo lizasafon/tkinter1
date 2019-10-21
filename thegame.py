002310d6e8aa8c6942c0700c1ce1c56911a2eb99
lizasafon <lizabusysafonova@gmail.com>
2019-10-21 21:58:53 +0300

    fkrint(res[0], 'of', res[1], 'by', name, file=results) results.close()e for your changes. Lines starting

--- thegame.py
+++ thegame.py
@@ -43,6 +43,7 @@ def new_ball_3():
     global o3
     o3 = canv.create_oval(x3 - r3, y3 - r3, x3 + r3, y3 + r3, fill=c3, width=0)
 
+
  # функция движения
 def move(object, x, y, r, c, v_x, v_y):
 	canv.delete(object)
@@ -60,7 +61,45 @@ def move(object, x, y, r, c, v_x, v_y):
 
 	return obj, x, y, v_x, v_y
 
- # функция счета очков
+ # своя мишень
+
+def own_target():
+	global x4, y4, r4
+	x4 = rnd(100, 700)
+	y4 = rnd(100, 500)
+	r4 = rnd(30, 50)
+	c4 = choice(colors)
+	global o4
+	o4 = canv.create_rectangle(x4 - r4, y4 - r4, x4 + r4, y4 + r4, fill=c4, width=5)
+
+ # закон движения мишени
+def distort(x, y, f=0, b=1, c=1, x0=0, y0=0, x1=0, y1=0):
+	global maxX, minX
+	f, b, c, x0, y0, x1, y1 = parametrs 
+	resX = c*((x-x0)*m.cos(f)-(y-y0)*m.sin(f))+x1
+	maxX = max(maxX, resX)
+	minX = min(minX, resX)
+	return(resX , b*((x-x0)*m.sin(f)+(y-y0)*m.cos(f))+y1)
+
+'''
+def own_move():
+	T=0
+v=1
+t=0
+while t<10:
+	a = house()
+	time.sleep(0.01)
+	parametrs[2] = abs(m.sin(t))
+	if (maxX>1100 or minX<0) and T>20:
+		v*=(-1)
+		T=0
+	parametrs[5]+=v
+	t+=0.1
+	T+=1
+	print(parametrs[5])
+'''
+
+ # функция счета очков + условие попадания в шар
 z1, z2, z3 = 0, 0, 0
 def count(event, txt):
 	global z1, z2, z3
@@ -73,7 +112,7 @@ def count(event, txt):
 	canv.delete('text')
 	canv.create_text(775, 15, text=txt, tag='text')
 
-# процесс запуска count
+ # процесс запуска count
 
 def click(event):
 	count(event, z1 + z2 + z3 + 1)
@@ -88,6 +127,11 @@ def f3(event):
 	click(event)
 
 # просто потому, что можем
+def add_results(name, res):
+	results = open('results.txt', 'a')
+	print(1)
+	print(res, 'by', name, file=results)
+	results.close()
 
 canv.bind('<Button-1>', f1)
 
@@ -97,12 +141,20 @@ v_y1, v_y2, v_y3 = V * (random() - 0.5), V * (random() - 0.5), V * (random() - 0
 new_ball_1()
 new_ball_2()
 new_ball_3()
-while True:
-	o1, x1, y1, v_x1, v_y1 = move(o1, x1, y1, r1, c1, v_x1, v_y1)
-	o2, x2, y2, v_x2, v_y2 = move(o2, x2, y2, r2, c2, v_x2, v_y2)
-	o3, x3, y3, v_x3, v_y3 = move(o3, x3, y3, r3, c3, v_x3, v_y3)
-	root.update()
-	time.sleep(0.013)
+own_target()
+try:
+
+	while True:
+		o1, x1, y1, v_x1, v_y1 = move(o1, x1, y1, r1, c1, v_x1, v_y1)
+		o2, x2, y2, v_x2, v_y2 = move(o2, x2, y2, r2, c2, v_x2, v_y2)
+		o3, x3, y3, v_x3, v_y3 = move(o3, x3, y3, r3, c3, v_x3, v_y3)
+		root.update()
+		time.sleep(0.013)
+except:
+	print('Enter your name:', end = '\n')
+	N = input()
+	add_result(N, count)
+
 
 
 
